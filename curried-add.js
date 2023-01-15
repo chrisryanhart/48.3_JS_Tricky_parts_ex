5896479c4294a665738dde5ab471133c31b94087
rrghiker <chris.ryan.hart1@gmail.com>
2023-01-15 10:25:28 -0500

    exercise complete

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,18 @@
 function curriedAdd(total) {
+    let sum = 0;
 
+    if(total === undefined) return 0;
+
+    if(total !== undefined){
+        return function addVal(num){
+            if(num === undefined){
+                return total;
+            }else{
+                total = total + num;
+                return addVal;
+            }
+        }
+    }
 }
 
 module.exports = { curriedAdd };
